830c06cb9820ffe87825387e5e0c400b05e04fb1
zenkinoleg <icis@mail.ru>
2020-09-23 12:10:35 +0300

    remove bad-local from index

--- common/config/bad-local.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "I'm a bad local file which shouldn't be placed into the repo in the first place!";
-echo "Updated!";
-echo "Updated one more time, but .gitignore ignores me. Mwahaha!!!";
